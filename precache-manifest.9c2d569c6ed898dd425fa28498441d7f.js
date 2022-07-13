3469192d194b48d599388b108b75463843fda8a2
maisapreis <maisapierinipreis@gmail.com>
2022-07-13 17:09:28 -0300

    My Website!
    My website portfolio.

---
+++ precache-manifest.9c2d569c6ed898dd425fa28498441d7f.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = [
+  {
+    "revision": "409613aed013b76d398c",
+    "url": "/static/css/main.00c9fc83.chunk.css"
+  },
+  {
+    "revision": "409613aed013b76d398c",
+    "url": "/static/js/main.409613ae.chunk.js"
+  },
+  {
+    "revision": "517527aa6758397229f0",
+    "url": "/static/js/1.517527aa.chunk.js"
+  },
+  {
+    "revision": "0b06d869da9cb1822105",
+    "url": "/static/css/2.b12ccde4.chunk.css"
+  },
+  {
+    "revision": "0b06d869da9cb1822105",
+    "url": "/static/js/2.0b06d869.chunk.js"
+  },
+  {
+    "revision": "997caaeffdf8a6b07656",
+    "url": "/static/js/runtime~main.997caaef.js"
+  },
+  {
+    "revision": "f0b2fde933f4e4c4d8ee1191ea1bd4d6",
+    "url": "/static/media/bg-lamp.f0b2fde9.jpg"
+  },
+  {
+    "revision": "412dc857863e2a0be78260ca5ad3c0ec",
+    "url": "/static/media/back-btn.412dc857.png"
+  },
+  {
+    "revision": "7c1d6c6344e0332cd82dc3533ba1d89c",
+    "url": "/static/media/angular.7c1d6c63.jpg"
+  },
+  {
+    "revision": "43f28cdae8778ba2ea9399cf51ab2162",
+    "url": "/static/media/htmlCss.43f28cda.jpg"
+  },
+  {
+    "revision": "da6a7dbb635220dd77ffbec5f576b8c9",
+    "url": "/static/media/js.da6a7dbb.jpg"
+  },
+  {
+    "revision": "3e575bf7e89da1b4ffb521dbfdd888ce",
+    "url": "/static/media/mongo.3e575bf7.jpg"
+  },
+  {
+    "revision": "dde1d81be565d0431bd18178a31c28e7",
+    "url": "/static/media/mySQL.dde1d81b.jpg"
+  },
+  {
+    "revision": "29d0924b1711cf0b4b1e0ed50f226da0",
+    "url": "/static/media/postgres.29d0924b.jpg"
+  },
+  {
+    "revision": "ee626790b11241a2d4ea18b765952e2c",
+    "url": "/static/media/react.ee626790.jpg"
+  },
+  {
+    "revision": "18e847462aab46485b7c13fea6b20f11",
+    "url": "/static/media/ts.18e84746.jpg"
+  },
+  {
+    "revision": "87d5840d748f60b74dcf6b477f0ea1c7",
+    "url": "/static/media/vue.87d5840d.jpg"
+  },
+  {
+    "revision": "2069df505d2e3701e95401317895a0ea",
+    "url": "/static/media/node.2069df50.jpg"
+  },
+  {
+    "revision": "351427068cd12aff901337654774246e",
+    "url": "/static/media/bg-laptop.35142706.jpg"
+  },
+  {
+    "revision": "758d88d934757a4e7dd384b97574d04e",
+    "url": "/static/media/code-eye2.758d88d9.jpg"
+  },
+  {
+    "revision": "63796933e218f97e91eb31e3089bb2d5",
+    "url": "/static/media/code-lamp.63796933.jpg"
+  },
+  {
+    "revision": "dee6a92636e526eaab30028272dda1a0",
+    "url": "/static/media/bg-projects.dee6a926.jpg"
+  },
+  {
+    "revision": "3c53029a63484e4820f6e2a11d093b2d",
+    "url": "/static/media/email.3c53029a.jpg"
+  },
+  {
+    "revision": "dcd7a6f5402ee41d96b4d409738d1703",
+    "url": "/static/media/whatsapp.dcd7a6f5.jpg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "4755c101cb0dcca5bf53ebcfe3b54b24",
+    "url": "/index.html"
+  }
+];
