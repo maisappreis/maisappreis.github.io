985d80d98fe7da07255c48bca4c7e67e2b9dd7a0
Maisa Preis <maisapierinipreis@gmail.com>
2023-12-07 08:51:26 -0300

    feat: Improvements was made in Portfolio area, like to add href, and add pics, and style changes.

--- frontend/src/components/portfolio/Portfolio.jsx
+++ frontend/src/components/portfolio/Portfolio.jsx
@@ -1,20 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../main/App.css";
 import "./Portfolio.css";
+import Gym from "../../assets/gym.png";
 import lampPic from "../../assets/code-lamp.png";
 
-const Portfolio = props =>
-    <section id="portfolio">
-        <div className="portfolio-area projects-area">
-            <div className="projects-overlay">
-                <h3 className="projects-subtitle">PORTFOLIO</h3>
-                <div className="projects-flex">
-                    <img className="proj-img" src={lampPic} alt="Lamp" />
-                    <img className="proj-img" src={lampPic} alt="Lamp" />
-                    <img className="proj-img" src={lampPic} alt="Lamp" />
+const Portfolio = (props) => {
+    const [isHovered, setIsHovered] = useState(false);
+
+    return (
+        <section id="portfolio">
+            <div className="portfolio-area projects-area">
+                <div className="projects-overlay">
+                    <h3 className="projects-subtitle">PORTFOLIO</h3>
+                    <div className="projects-flex">
+                        <div className="proj-container"
+                            onMouseEnter={() => setIsHovered(true)}
+                            onMouseLeave={() => setIsHovered(false)}
+                            >
+                            <img className="proj-img" src={Gym} alt="Gym System" />
+                                {isHovered && (
+                                <a href="https://github.com/maisappreis/registration-system-upfit" target="_blank"
+                                    rel="noopener noreferrer" className="hover-box">
+                                    <p className="hover-text">An ERP System for a Gym Company</p>
+                                </a>
+                            )}
+                        </div>
+                        <div className="proj-container"
+                            onMouseEnter={() => setIsHovered(true)}
+                            onMouseLeave={() => setIsHovered(false)}
+                            >
+                            <img className="proj-img" src={lampPic} alt="Lamp" />
+                                {isHovered && (
+                                <a href="https://github.com/maisappreis/" target="_blank"
+                                    rel="noopener noreferrer" className="hover-box">
+                                    <p className="hover-text">An ERP System for a Dental Clinic</p>
+                                </a>
+                            )}
+                        </div>
+                        <div className="proj-container"
+                            onMouseEnter={() => setIsHovered(true)}
+                            onMouseLeave={() => setIsHovered(false)}
+                            >
+                            <img className="proj-img" src={lampPic} alt="Lamp" />
+                                {isHovered && (
+                                <a href="https://github.com/maisappreis/supply-chain" target="_blank"
+                                    rel="noopener noreferrer" className="hover-box">
+                                    <p className="hover-text">An Supply Chain System</p>
+                                </a>
+                            )}
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-    </section>
+        </section>
+    )
+}
 
 export default Portfolio;
