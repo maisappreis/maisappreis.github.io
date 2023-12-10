7c937f1140a96cd6126d6c6009c2bc5e79b81318
Maisa Preis <maisapierinipreis@gmail.com>
2023-12-10 09:59:32 -0300

    feat: Some progress bars were applied to the Skills area.

--- frontend/src/components/skills/Skills.jsx
+++ frontend/src/components/skills/Skills.jsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React from 'react';
 import "../../main/App.css";
 import "./Skills.css";
+import ProgressBar from "../progress/ProgressBar";
 
-const Skills = props =>
-    <div className="skills-area container-fluid" id="skills">
-        <h3 className="skills-subtitle">SKILLS</h3>
-        <div className="skills-flex">
+const Skills = () => {
+  
+    return (
+        <div className="skills-area container-fluid" id="skills">
+            <h3 className="skills-subtitle">SKILLS</h3>
+            <div className="skills-row">
+                <div className="skills-column">
+                    <ProgressBar label="HTML5" targetProgress={85} />
+                    <ProgressBar label="CSS3" targetProgress={80} />
+                    <ProgressBar label="Vue.js" targetProgress={75} />
+                    <ProgressBar label="JavaScript" targetProgress={70} />
+                </div>
+                <div className="skills-column">
+                    <ProgressBar label="SQL" targetProgress={65} />
+                    <ProgressBar label="Python" targetProgress={60} />
+                    <ProgressBar label="Django" targetProgress={50} />
+                </div>
+            </div>
         </div>
-    </div>
-
-export default Skills;
+    );
+  };
+  
+  export default Skills;
