33f12bb6dda496b838d0c9d27a5d9d9413f91ecd
Maisa Preis <maisapierinipreis@gmail.com>
2024-09-11 19:25:46 -0300

    Pushing changes to deploy

---
+++ precache-manifest.c1ce871dcf259e5e20e62692c18a609b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "05cf2f80fa7d9c9f2978",
+    "url": "/static/css/main.eadab4ff.chunk.css"
+  },
+  {
+    "revision": "05cf2f80fa7d9c9f2978",
+    "url": "/static/js/main.dd8373dd.chunk.js"
+  },
+  {
+    "revision": "3a6d8df43e891d1a2c55",
+    "url": "/static/js/runtime~main.95a8b211.js"
+  },
+  {
+    "revision": "9b8e4f1a498e00a4e9de",
+    "url": "/static/css/2.2bd7dc97.chunk.css"
+  },
+  {
+    "revision": "9b8e4f1a498e00a4e9de",
+    "url": "/static/js/2.ce9a14b6.chunk.js"
+  },
+  {
+    "revision": "5e1c4a9743c24123230f",
+    "url": "/static/js/3.02c98a0d.chunk.js"
+  },
+  {
+    "revision": "945735f5940bea086ac92d74e370524d",
+    "url": "/static/media/bg-dark.945735f5.jpg"
+  },
+  {
+    "revision": "0cf9d98d659bdde87c6cbf3b1e16ccc5",
+    "url": "/static/media/code-lamp.0cf9d98d.png"
+  },
+  {
+    "revision": "dd0f747f93050cf02616120da5d964bd",
+    "url": "/static/media/gym.dd0f747f.png"
+  },
+  {
+    "revision": "eeb0c9965ffeb7ebacfc048e7249bd66",
+    "url": "/static/media/django.eeb0c996.png"
+  },
+  {
+    "revision": "224f82f9d3d9bf2e2d393821f9dd1b75",
+    "url": "/static/media/dental-clinic.224f82f9.png"
+  },
+  {
+    "revision": "562db397bdfa18decc56bcb9bd0e4872",
+    "url": "/index.html"
+  }
+];
